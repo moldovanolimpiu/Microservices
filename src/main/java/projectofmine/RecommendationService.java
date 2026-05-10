7f400cc2a8d228ea16cdc318eaf318e65cacab8d
moldovanolimpiu <moldovan.vlad22@gmail.com>
2026-05-10 23:36:51 +0300

    RecommendationService: added faults based on rand

--- src/main/java/projectofmine/RecommendationService.java
+++ src/main/java/projectofmine/RecommendationService.java
@@ -6,17 +6,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RecommendationService {
     private Connection con;
+    private Random rand = new Random();
 
     public RecommendationService(Connection con) {
         this.con = con;
     }
 
-    public List<Movie> fetchRecommendedMovies(String queryGenre) throws SQLException {
+    public List<Movie> fetchRecommendedMovies(String queryGenre) throws SQLException, InterruptedException {
         List<Movie> movies = new ArrayList<Movie>();
         String sql = "SELECT * FROM movies WHERE genre = ?";
+
+        int chance = rand.nextInt(100);
+        System.out.println(chance);
+
+        if(chance < 20){
+            throw new RuntimeException("503 Service Unavailable");
+        }
+
+        if(chance < 40){
+            int delay = 3000 + rand.nextInt(7000);
+            Thread.sleep(delay);
+        }
+
         try (PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, queryGenre);
             try (ResultSet rs = ps.executeQuery()) {
@@ -26,11 +41,8 @@ public class RecommendationService {
                     boolean def_rec = rs.getBoolean("default_rec");
                     movies.add(new Movie(title, genre, def_rec));
                 }
-            }catch (Exception e) {
-                e.printStackTrace();
             }
-        }catch (SQLException e) {
-            e.printStackTrace();
+
         }
         return movies;
     }
